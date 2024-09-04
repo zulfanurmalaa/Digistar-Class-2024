9fdffe00bd377374f5d6515810fbf5d9b717084f
zulfanurmalaa <151809427+zulfanurmalaa@users.noreply.github.com>
2024-09-05 01:58:07 +0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Products from './Products';  // Import komponen Products
+
+const App = () => {
+  return (
+    <div>
+      <Products />  {/* Render komponen Products */}
+    </div>
+  );
+};
+
+export default App;
